55941a88922ee49a62d7ceb621aa02cc7e1cb6ae
Yusuke Sato <liberal.engineer113@gmail.com>
2021-02-25 17:29:38 +0900

    Add React Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  before_action :fake_load
+
+  def fake_load
+    sleep(1)
+  end
+
 end
---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import{ Restaurants } from './containers/Restaurants.jsx'; 
+import{ Foods } from './containers/Foods.jsx'; 
+import{ Orders } from './containers/Orders.jsx'; 
+
+function App() {
+
+  return (
+    <Router>
+      <Switch>
+        {/* 店舗一覧ページ */}
+        <Route
+          exact
+          path="/restaurants"
+        >
+          <Restaurants />
+        </Route>
+        {/* フード一覧 */}
+        <Route
+          exact
+          path="/restaurants/:restaurantsId/foods"
+          render={({ match }) => 
+            <Foods 
+              match={match}
+            />
+          }
+        />
+        {/* 注文ページ */}
+        <Route
+          exact
+          path="/orders"
+        >
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
